a469f12934149fe1cf02290c7d842e0f12d8b9e1
Shugo Ishige <sishige@c1r6s4.42tokyo.jp>
2024-08-24 19:42:33 +0900

    CREAT pipex.h

---
+++ pipex.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sishige <sishige@student.42tokyo.j>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 17:41:08 by sishige           #+#    #+#             */
+/*   Updated: 2024/08/23 18:38:26 by sishige          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# define R 0
+# define W 1
+# define I 0
+# define O 1
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+
+#endif
