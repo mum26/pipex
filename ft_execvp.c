e15df4eaba7d78fcc64b60bf5a0024b256e68294
Shugo Ishige <sishige@c1r6s4.42tokyo.jp>
2024-08-24 23:38:57 +0900

    MOD

--- ft_execvp.c
+++ ft_execvp.c
@@ -1,24 +1,65 @@
-extern char	**environ;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_execvp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sishige <sishige@student.42tokyo.j>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 19:48:36 by sishige           #+#    #+#             */
+/*   Updated: 2024/08/24 23:07:17 by sishige          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ft_execvp(char *cmd, char *argv[])
+#include "pipex.h"
+
+static void	cleanup(char **substrs)
 {
-	char	**dir;
-	char	*path;
+	char **temp = substrs;
 
-	dir = ft_split(ft_getenv("PATH"), ':');
-	if (!dir)
-		return (-1);
-	while (*dir)
+	while (*temp)
 	{
-		path = ft_strjoin(*dir, cmd);
-		if (access(path, X_OK))
-		{
-			// path が free() できないけどいいんだろうか？
-			if (execve(path, argv, environ) < 0)
-				peer_exit("execve");
-		}
-		free (path);
-		dir++;
+		free(*temp);
+		*temp = NULL;
+		temp++;
 	}
-	return (-1);
+	free(substrs);
+	substrs = NULL;
 }
+
+int	ft_execvp(char *file, char *const argv[])
+{
+	char	**dirs;
+	char	*path;
+	char	*file_path;
+	size_t	i;
+
+	dirs = ft_split(ft_getenv("PATH"), ':');
+	if (!dirs)
+		return (-1);
+	file_path = ft_strjoin("/", file);
+	i = 0;
+	while (dirs[i])
+	{
+		path = ft_strjoin(dirs[i], file_path);
+		if (!access(path, X_OK))
+			break ;
+		free(path);
+		path = NULL;
+		i++;
+	}
+	execve(path, argv, environ);
+	return (cleanup(dirs), free(file_path), free(path), -1);
+}
+
+//__attribute__((destructor)) static void destructor(void)
+//{
+//	system("leaks -q a.out");
+//}
+//
+//int	main(int argc, char *argv[])
+//{
+//	(void)argc;
+//	ft_execvp(argv[1], &argv[1]);
+//	perror("ft_execvp");
+//	exit(1);
+//}
